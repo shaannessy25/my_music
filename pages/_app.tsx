e5ecafb82656ccd7287a905502796cca8ca4c8d2
shaannessy25 <shaan.hurley25@gmail.com>
2022-05-25 18:41:14 -0700

    adding menus

---
+++ pages/_app.tsx
@@ -0,0 +1,41 @@
+import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "reset-css";
+import PlayerLayout from "../components/playerLayout";
+
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "#F5F5F5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+});
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <PlayerLayout>
+        <Component {...pageProps} />
+      </PlayerLayout>
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;
